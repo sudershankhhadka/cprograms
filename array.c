a9371b2e49eeb1dce899569113b6f21fc208e3ca
sudershankhhadka <sudershankhadka10@gmail.com>
2023-02-09 21:40:18 +0545

    2023-02-09

--- array.c
+++ array.c
@@ -19,18 +19,54 @@
 //Program that reads 10 float number entered from the user and displays it on the screen
 
 
-#include<stdio.h>
-int main()
-{
-    int i;
-    float number[10];
-    printf("enter the numbers");
-    for (i=0;i<=9;i++){
-        scanf("%f",&number[i]);
-    }
-    printf("The Numbers are \n");
-    for(i=0;i<=9;i++){
-        printf("%d %f \n",i+1,number[i]);
-    }
-return 0;
-}
+// #include<stdio.h>
+// int main()
+// {
+//     int i;
+//     float number[10];
+//     printf("enter the numbers");
+//     for (i=0;i<=9;i++){
+//         scanf("%f",&number[i]);
+//     }
+//     printf("The Numbers are \n");
+//     for(i=0;i<=9;i++){
+//         printf("%d %f \n",i+1,number[i]);
+//     }
+// return 0;
+// }
+
+
+// //program that accepts marks for 5 subjects and displays sum and average
+
+// #include<stdio.h>
+// int main()
+// {
+//     int marks[5],sum=0,i;
+//     float avg;
+//     for(i=0;i<5;i++){
+//         printf("Enter the marks obtained %d",i+1);
+//         scanf("%d",&marks[i]);    
+//     }
+    
+
+
+// }
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+// 	int a=10,b=2, sum;
+// 	scanf("%d%d",&a,&b);
+// 	sum= a+b;
+// 	printf("sum=%d",sum);
+// }
+// #include<stdio.h>
+// int main()
+// {
+// 	int a=10,b=2, sum;
+// 	scanf("%d%d",&a,&b);
+// 	sum= a+b;
+// 	printf("sum=%d",sum);
+// }
